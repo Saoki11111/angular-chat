ca8d934814626269298131cc77112d4d4a7f9e06
Saoki11111 <t.aoki11111@gmail.com>
2023-03-29 12:55:13 +0900

    カスタムパイプの定義

--- src/app/pipes/comment-date.pipe.ts
+++ src/app/pipes/comment-date.pipe.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,8 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CommentDatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, ...args: string[]): string {
+    const format =args[0] || "yyyy年MM月dd日 HH:mm";
+    return formatDate(value, format, 'en-US');
   }
 
 }
