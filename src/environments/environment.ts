70589ae0cad1176ebb981abae073b57551ea7a1c
Saoki11111 <t.aoki11111@gmail.com>
2023-03-30 17:38:44 +0900

    firebase 連携用の設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,21 @@
 
 export const environment = {
   production: false
+  firebase: {
+    apiKey: "AIzaSyCWav5D8Opf3-hNoha-OekYjLtSp2rziTQ",
+    authDomain: "angular-chat-3aafa.firebaseapp.com",
+    projectId: "angular-chat-3aafa",
+    storageBucket: "angular-chat-3aafa.appspot.com",
+    messagingSenderId: "319660529307",
+    appId: "1:319660529307:web:41a3a4ea29e552ee66583e"
+  }
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
+* For easier debugging in development mode, you can import the following file
+* to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+*
+* This import should be commented out in production mode because it will have a negative impact
+* on performance if an error is thrown.
+*/
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
